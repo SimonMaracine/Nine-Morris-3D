f57cde9e964cf3522fb021b41972f42f89373287
SimonMaracine <simonmaracine@gmail.com>
2021-07-01 17:50:57 +0300

    Tweak logging format

--- src/logging.cpp
+++ src/logging.cpp
@@ -7,7 +7,7 @@ SPDLOG_WARN, SPDLOG_ERROR, SPDLOG_CRITICAL
 
 namespace logging {
     void init() {
-        spdlog::set_pattern("[%l] %H:%M:%S %v");
+        spdlog::set_pattern("[%l] [%H:%M:%S] %v");
         spdlog::set_level(spdlog::level::trace);
     }
 }
