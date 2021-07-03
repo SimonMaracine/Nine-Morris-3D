2fe9b96e4431b1bf2a3a01f28c008e1c67cf46ae
SimonMaracine <simonmaracine@gmail.com>
2021-07-03 23:49:03 +0300

    Begin implementing a perspective camera

---
+++ src/opengl/renderer/camera.cpp
@@ -0,0 +1,13 @@
+#include "opengl/renderer/camera.h"
+
+PerspectiveCamera::PerspectiveCamera(float left, float right, float bottom, float top) {
+
+}
+
+void PerspectiveCamera::update() {
+
+}
+
+void PerspectiveCamera::recalculate_view_matrix() {
+
+}
