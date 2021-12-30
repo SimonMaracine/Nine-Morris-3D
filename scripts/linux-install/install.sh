cfa1b94dd39fbfde03bf2bad1158d2fdc3c8a178
SimonMaracine <simonmaracine@gmail.com>
2021-12-30 23:10:21 +0200

    Add icons of different sizes and update Linux installer

--- scripts/linux-install/install.sh
+++ scripts/linux-install/install.sh
@@ -5,10 +5,13 @@
 APP_NAME="ninemorris3d"
 EXECUTABLE_NAME="Nine-Morris-3D"
 HELP="HELP_LINUX.txt"
+SHORTCUT="ninemorris3d.desktop"
+ICON="ninemorris3d.png"
 
 EXECUTABLE_DESTINATION_FOLDER="/usr/games"
 DATA_DESTINATION_FOLDER="/usr/share/$APP_NAME"
 SHORTCUT_FOLDER="/usr/share/applications"
+ICONS_FOLDER="/usr/share/icons/hicolor"
 
 function check_failure() {
     if [ $? != 0 ]; then
@@ -17,38 +20,56 @@ function check_failure() {
     fi
 }
 
+function copy_icon() {
+    cp $current_dir/data/icons/$1/$ICON $ICONS_FOLDER/$1/apps/$ICON
+    check_failure
+    echo "Copied $current_dir/data/icons/$1/$ICON to $ICONS_FOLDER/$1/apps/$ICON"
+}
+
 current_dir=$(pwd)
 echo "Working directory: $current_dir"
 
+# Executable
 cp $current_dir/$EXECUTABLE_NAME $EXECUTABLE_DESTINATION_FOLDER/$EXECUTABLE_NAME
 check_failure
 echo "Copied $current_dir/$EXECUTABLE_NAME to $EXECUTABLE_DESTINATION_FOLDER/$EXECUTABLE_NAME"
 
+# Data folder
 mkdir -p $DATA_DESTINATION_FOLDER/
 cp -r $current_dir/data/ $DATA_DESTINATION_FOLDER/data/
 check_failure
 echo "Copied $current_dir/data/ to $DATA_DESTINATION_FOLDER/data/"
 
-touch $SHORTCUT_FOLDER/$APP_NAME.desktop
+# Icons
+copy_icon 512x512
+copy_icon 256x256
+copy_icon 128x128
+copy_icon 64x64
+copy_icon 32x32
+
+# Shortcut
+touch $SHORTCUT_FOLDER/$SHORTCUT
 check_failure
-cat << EOF > $SHORTCUT_FOLDER/$APP_NAME.desktop
+cat << EOF > $SHORTCUT_FOLDER/$SHORTCUT
 [Desktop Entry]
 Version=0.1.0
 Terminal=false
 Exec=$EXECUTABLE_NAME
-Icon=$DATA_DESTINATION_FOLDER/data/icons/icon.png
+Icon=$APP_NAME
 Type=Application
 Categories=Game;BoardGame;
 Name=Nine Morris 3D
 Comment=A Nine Men's Morris Implementation In 3D
 EOF
 check_failure
-echo "Created shortcut $SHORTCUT_FOLDER/$APP_NAME.desktop"
+echo "Created shortcut $SHORTCUT_FOLDER/$SHORTCUT"
 
+# Uninstall script
 cp $current_dir/uninstall.sh $DATA_DESTINATION_FOLDER/uninstall.sh
 check_failure
 echo "Copied $current_dir/uninstall.sh to $DATA_DESTINATION_FOLDER/uninstall.sh"
 
+# Help file
 cp $current_dir/$HELP $DATA_DESTINATION_FOLDER/$HELP
 check_failure
 echo "Copied $current_dir/$HELP to $DATA_DESTINATION_FOLDER/$HELP"
--- scripts/linux-install/uninstall.sh
+++ scripts/linux-install/uninstall.sh
@@ -3,10 +3,13 @@
 APP_NAME="ninemorris3d"
 EXECUTABLE_NAME="Nine-Morris-3D"
 HELP="HELP_LINUX.txt"
+SHORTCUT="ninemorris3d.desktop"
+ICON="ninemorris3d.png"
 
 EXECUTABLE_DESTINATION_FOLDER="/usr/games"
 DATA_DESTINATION_FOLDER="/usr/share/$APP_NAME"
 SHORTCUT_FOLDER="/usr/share/applications"
+ICONS_FOLDER="/usr/share/icons/hicolor"
 
 error=false
 
@@ -19,14 +22,25 @@ function check_failure() {
     fi
 }
 
+function remove_icon() {
+    rm -f $ICONS_FOLDER/$1/apps/$ICON
+    check_failure "Removed $ICONS_FOLDER/$1/apps/$ICON"
+}
+
 rm -f $EXECUTABLE_DESTINATION_FOLDER/$EXECUTABLE_NAME
 check_failure "Removed $EXECUTABLE_DESTINATION_FOLDER/$EXECUTABLE_NAME"
 
 rm -rf $DATA_DESTINATION_FOLDER/
 check_failure "Removed $DATA_DESTINATION_FOLDER/"
 
-rm -f $SHORTCUT_FOLDER/$APP_NAME.desktop
-check_failure "Removed $SHORTCUT_FOLDER/$APP_NAME.desktop"
+remove_icon 512x512
+remove_icon 256x256
+remove_icon 128x128
+remove_icon 64x64
+remove_icon 32x32
+
+rm -f $SHORTCUT_FOLDER/$SHORTCUT
+check_failure "Removed $SHORTCUT_FOLDER/$SHORTCUT"
 
 rm -rf ${HOME}/.$APP_NAME
 check_failure "Removed ${HOME}/.$APP_NAME"
