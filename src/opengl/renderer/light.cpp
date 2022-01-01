e331d7363362858d41515c981fc161ccd944fa07
SimonMaracine <simonmaracine@gmail.com>
2022-01-01 20:53:25 +0200

    Remake skyboxes again (blur them)

--- src/opengl/renderer/light.cpp
+++ src/opengl/renderer/light.cpp
@@ -3,15 +3,15 @@
 #include "opengl/renderer/light.h"
 
 const Light LIGHT_FIELD = {
-    glm::vec3(-15.1f, 11.8f, 9.1f),
+    glm::vec3(5.7f, 8.4f, 12.4f),
     glm::vec3(0.3f),
     glm::vec3(1.0f),
     glm::vec3(1.0f)
 };
 
 const Light LIGHT_AUTUMN = {
-    glm::vec3(7.0f, 12.0f, 4.4f),
+    glm::vec3(-4.4f, 11.0f, 6.4f),
     glm::vec3(0.15f),
     glm::vec3(1.0f),
-    glm::vec3(1.0f)
+    glm::vec3(0.9f)
 };
