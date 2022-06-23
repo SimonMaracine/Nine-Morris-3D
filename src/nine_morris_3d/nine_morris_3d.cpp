2ab76193387abb0319128447965a76b6aaa5fcdc
SimonMaracine <simonmaracine@gmail.com>
2022-06-23 11:27:14 +0300

    Incresase size of the default GUI font

--- src/nine_morris_3d/nine_morris_3d.cpp
+++ src/nine_morris_3d/nine_morris_3d.cpp
@@ -46,23 +46,24 @@ NineMorris3D::NineMorris3D()
 
     using namespace assets;
     using namespace encryption;
+    using namespace paths;
 
     // Load and set icons
     {
         std::array<std::unique_ptr<TextureData>, 5> icons = {
-            std::make_unique<TextureData>(paths::path_for_assets(ICON_512), false),
-            std::make_unique<TextureData>(paths::path_for_assets(ICON_256), false),
-            std::make_unique<TextureData>(paths::path_for_assets(ICON_128), false),
-            std::make_unique<TextureData>(paths::path_for_assets(ICON_64), false),
-            std::make_unique<TextureData>(paths::path_for_assets(ICON_32), false)
+            std::make_unique<TextureData>(path_for_assets(ICON_512), false),
+            std::make_unique<TextureData>(path_for_assets(ICON_256), false),
+            std::make_unique<TextureData>(path_for_assets(ICON_128), false),
+            std::make_unique<TextureData>(path_for_assets(ICON_64), false),
+            std::make_unique<TextureData>(path_for_assets(ICON_32), false)
         };
 
         window->set_icons<5>(icons);
     }
 
     // Load and set cursors
-    arrow_cursor = window->add_cursor(std::make_unique<TextureData>(convert(paths::path_for_assets(ARROW_CURSOR)), false), 4, 1);
-    cross_cursor = window->add_cursor(std::make_unique<TextureData>(convert(paths::path_for_assets(CROSS_CURSOR)), false), 8, 8);
+    arrow_cursor = window->add_cursor(std::make_unique<TextureData>(convert(path_for_assets(ARROW_CURSOR)), false), 4, 1);
+    cross_cursor = window->add_cursor(std::make_unique<TextureData>(convert(path_for_assets(CROSS_CURSOR)), false), 8, 8);
 
     // Setup scene framebuffer
     {
@@ -97,18 +98,18 @@ NineMorris3D::NineMorris3D()
         ImVector<ImWchar> ranges;
         builder.BuildRanges(&ranges);
 
-        io.FontDefault = io.Fonts->AddFontFromFileTTF(paths::path_for_assets(OPEN_SANS_FONT).c_str(), 20.0f);
-        data.imgui_info_font = io.Fonts->AddFontFromFileTTF(paths::path_for_assets(OPEN_SANS_FONT).c_str(), 16.0f);
-        data.imgui_windows_font = io.Fonts->AddFontFromFileTTF(paths::path_for_assets(OPEN_SANS_FONT).c_str(), 24.0f,
+        io.FontDefault = io.Fonts->AddFontFromFileTTF(path_for_assets(OPEN_SANS_FONT).c_str(), 21.0f);
+        data.imgui_info_font = io.Fonts->AddFontFromFileTTF(path_for_assets(OPEN_SANS_FONT).c_str(), 16.0f);
+        data.imgui_windows_font = io.Fonts->AddFontFromFileTTF(path_for_assets(OPEN_SANS_FONT).c_str(), 24.0f,
                 nullptr, ranges.Data);
         io.Fonts->Build();
     }
 
     // Load splash screen
-    data.splash_screen_texture = Texture::create(convert(paths::path_for_assets(SPLASH_SCREEN_TEXTURE)), true);
+    data.splash_screen_texture = Texture::create(convert(path_for_assets(SPLASH_SCREEN_TEXTURE)), true);
 
     // Load and create this font
-    data.good_dog_plain_font = std::make_shared<Font>(paths::path_for_assets(GOOD_DOG_PLAIN_FONT), 50.0f, 5, 180, 40, 512);
+    data.good_dog_plain_font = std::make_shared<Font>(path_for_assets(GOOD_DOG_PLAIN_FONT), 50.0f, 5, 180, 40, 512);
     data.good_dog_plain_font->begin_baking();  // TODO maybe move part of texture baking to thread
     data.good_dog_plain_font->bake_characters(32, 127);
     data.good_dog_plain_font->end_baking();
