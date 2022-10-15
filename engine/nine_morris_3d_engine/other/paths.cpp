5ad4e664b3216acc8e21237091d8f5ebe00dccd8
SimonMaracine <simonmaracine@gmail.com>
2022-10-15 10:04:53 +0300

    Improve paths a bit

--- engine/nine_morris_3d_engine/other/paths.cpp
+++ engine/nine_morris_3d_engine/other/paths.cpp
@@ -2,6 +2,47 @@
 #include "nine_morris_3d_engine/other/paths.h"
 #include "nine_morris_3d_engine/other/user_data.h"
 
+#if defined(PLATFORM_GAME_DEBUG)
+    // Use relative path for both operating systems
+    static std::string path_for_logs_impl(std::string_view file) noexcept(false) {
+        return std::string(file);
+    }
+
+    static std::string path_for_saved_data_impl(std::string_view file) noexcept(false) {
+        return std::string(file);
+    }
+
+    static std::string path_for_assets_impl(std::string_view file_path) {
+        return std::string(file_path);
+    }
+#elif defined(PLATFORM_GAME_RELEASE)
+    #if defined(PLATFORM_GAME_LINUX)
+        static std::string path_for_logs_impl(std::string_view file) noexcept(false) {
+            return user_data::get_user_data_directory_path(app_name) + std::string(file);
+        }
+
+        static std::string path_for_saved_data_impl(std::string_view file) noexcept(false) {
+            return user_data::get_user_data_directory_path(app_name) + std::string(file);
+        }
+
+        static std::string path_for_assets_impl(std::string_view file_path) {
+            return std::string("/usr/share/") + app_name + "/" + std::string(file_path);
+        }
+    #elif defined(PLATFORM_GAME_WINDOWS)
+        static std::string path_for_logs_impl(std::string_view file) noexcept(false) {
+            return "C:\\Users\\" + user_data::get_username() + "\\Documents\\" + app_name + "\\" + std::string(file);
+        }
+
+        static std::string path_for_saved_data_impl(std::string_view file) noexcept(false) {
+            return user_data::get_user_data_directory_path(app_name) + std::string(file);
+        }
+
+        static std::string path_for_assets_impl(std::string_view file_path) {
+            return std::string(file_path);
+        }
+    #endif
+#endif
+
 namespace paths {
     static std::string app_name;
 
@@ -10,42 +51,14 @@ namespace paths {
     }
 
     std::string path_for_logs(std::string_view file) noexcept(false) {
-#if defined(PLATFORM_GAME_DEBUG)
-        // Use relative path for both operating systems
-        return std::string(file);
-#elif defined(PLATFORM_GAME_RELEASE)
-    #if defined(PLATFORM_GAME_LINUX)
-        return user_data::get_user_data_directory_path(app_name) + std::string(file);
-    #elif defined(PLATFORM_GAME_WINDOWS)
-        return "C:\\Users\\" + user_data::get_username() + "\\Documents\\" + app_name + "\\" + std::string(file);
-    #endif
-#endif
+        return path_for_logs_impl(file);
     }
 
     std::string path_for_saved_data(std::string_view file) noexcept(false) {
-#if defined(PLATFORM_GAME_DEBUG)
-        // Use relative path for both operating systems
-        return std::string(file);
-#elif defined(PLATFORM_GAME_RELEASE)
-    #if defined(PLATFORM_GAME_LINUX)
-        return user_data::get_user_data_directory_path(app_name) + std::string(file);
-    #elif defined(PLATFORM_GAME_WINDOWS)
-        return user_data::get_user_data_directory_path(app_name) + std::string(file);
-    #endif
-#endif
+        return path_for_saved_data_impl(file);
     }
 
     std::string path_for_assets(std::string_view file_path) {
-#if defined(PLATFORM_GAME_DEBUG)
-        // Use relative path for both operating systems
-        return std::string(file_path);
-#elif defined(PLATFORM_GAME_RELEASE)
-    #if defined(PLATFORM_GAME_LINUX)
-        return std::string("/usr/share/") + app_name + "/" + std::string(file_path);
-    #elif defined(PLATFORM_GAME_WINDOWS)
-        // Just use relative path
-        return std::string(file_path);
-    #endif
-#endif
+        return path_for_assets_impl(file_path);
     }
 }
