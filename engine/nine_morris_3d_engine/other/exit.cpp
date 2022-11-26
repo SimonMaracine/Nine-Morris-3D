ae9db2c18164b3f071016c7a93a831b72147163b
SimonMaracine <simonmaracine@gmail.com>
2022-11-26 22:37:55 +0200

    Gracefully clean up some memory when exiting on error

--- engine/nine_morris_3d_engine/other/exit.cpp
+++ engine/nine_morris_3d_engine/other/exit.cpp
@@ -1,7 +1,10 @@
 #include "nine_morris_3d_engine/other/exit.h"
 
+#include <GLFW/glfw3.h>
+
 namespace game_exit {
     void exit_critical() {
+        glfwTerminate();  // Gracefully clean up at least some memory
         exit(1);
     }
 }
