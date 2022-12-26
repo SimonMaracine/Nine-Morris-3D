8662b3a0d4c20beae31200318630ea6f46c8ee71
SimonMaracine <simonmaracine@gmail.com>
2022-12-26 11:18:05 +0200

    Make Windows build scripts work again and clean up CMakeLists.txt code

--- scripts/Run.bat
+++ scripts/Run.bat
@@ -1,15 +1,16 @@
 echo off
 
 cls
-cd ..
-cmake --build . --config Debug
+cd ..\build
+cmake --build . --config Debug -j4
 
 if %ERRORLEVEL% EQU 0 (
+    cd ..
     cls
-    Debug\Nine-Morris-3D.exe
+    build\Nine-Morris-3D\Debug\Nine-Morris-3D.exe
     cd scripts
 )
 
 if %ERRORLEVEL% NEQ 0 (
-    cd scripts
+    cd ..\scripts
 )
