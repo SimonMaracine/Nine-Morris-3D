a3a7fd2a2f5a4a473d8f1de96a887d3628440fc5
SimonMaracine <simonmaracine@gmail.com>
2022-12-26 12:40:09 +0200

    Clean up some code after all of this refactoring

--- scripts/Make_Encrypter.bat
+++ scripts/Make_Encrypter.bat
@@ -1,10 +1,5 @@
 echo off
 
-@REM cd ..\encrypter_helper
-@REM cmake . -D CMAKE_BUILD_TYPE=Release -A x64
-@REM cmake --build . --config Release
-@REM cd ..\scripts
-
 cd ..\build
-cmake --build . --target encrypter_helper --config Release
+cmake --build . --target encrypter_helper
 cd ..\scripts
